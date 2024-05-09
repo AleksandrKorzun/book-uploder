8a1d52cd9e529b27b67676c8c804966159a5c450
Oleksandr Korzun <oleksandrkorzun@Vlad-Danilochkin-Air13-M1.local>
2024-05-09 21:12:01 +0300

    fix: token

--- src/plugins/builder/admin/src/components/BooksTable/BooksRow/index.js
+++ src/plugins/builder/admin/src/components/BooksTable/BooksRow/index.js
@@ -1,6 +1,11 @@
 import {
   BaseCheckbox,
   Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  Flex,
+  Loader,
   Status,
   Td,
   Tr,
@@ -8,9 +13,13 @@ import {
 } from "@strapi/design-system";
 import React, { useEffect, useMemo, useState } from "react";
 import { addNewChapter, getMediaFoldersByPath } from "../../../utils/getBooks";
+import { ExclamationMarkCircle } from "@strapi/icons";
 
 const BooksRow = ({ folder, book }) => {
   const [localFolders, setLocalFolders] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [error, setError] = useState("");
 
   const isPublished = useMemo(() => book && book?.attributes?.chapters, [book]);
 
@@ -30,42 +39,51 @@ const BooksRow = ({ folder, book }) => {
 
   const buildBook = async () => {
     if (!book?.id) return;
-
-    const allChapters = await Promise.all(
-      localFolders.map(
-        async (local) =>
-          await getMediaFoldersByPath(local.path).then((data) => ({
-            lang: data.name,
-            files: data.files,
-          }))
-      )
-    );
-
-    const chaptersInBook = allChapters[0].files.length;
-
-    for (let index = 0; index < chaptersInBook; index++) {
-      const chapterOrder =
-        Number(allChapters[0].files[index].name.split("_")[0]) + 1;
-
-      // if book has chapter with same chapterOrder return
-      if (publishedChapterOrders.includes(chapterOrder)) continue;
-      const data = { bookId: book?.id, chapterOrder };
-
-      await Promise.all(
-        allChapters.map(async ({ lang, files }) => {
-          const docUrl = files.find(
-            ({ name }) => Number(name.split("_")[0]) + 1 === chapterOrder
-          )?.url;
-          const response = await fetch(docUrl);
-          const text = await response.text();
-
-          data[`doc${lang.toUpperCase()}`] = text.split("\n").map((p) => {
-            return { type: "paragraph", children: [{ type: "text", text: p }] };
-          });
-        })
+    try {
+      setLoader(true);
+      const allChapters = await Promise.all(
+        localFolders.map(
+          async (local) =>
+            await getMediaFoldersByPath(local.path).then((data) => ({
+              lang: data.name,
+              files: data.files,
+            }))
+        )
       );
 
-      await addNewChapter({ data });
+      const chaptersInBook = allChapters[0].files.length;
+
+      for (let index = 0; index < chaptersInBook; index++) {
+        const chapterOrder =
+          Number(allChapters[0].files[index].name.split("_")[0]) + 1;
+
+        // if book has chapter with same chapterOrder return
+        if (publishedChapterOrders.includes(chapterOrder)) continue;
+        const data = { bookId: book?.id, chapterOrder };
+
+        await Promise.all(
+          allChapters.map(async ({ lang, files }) => {
+            const docUrl = files.find(
+              ({ name }) => Number(name.split("_")[0]) + 1 === chapterOrder
+            )?.url;
+            const response = await fetch(docUrl);
+            const text = await response.text();
+
+            data[`doc${lang.toUpperCase()}`] = text.split("\n").map((p) => {
+              return {
+                type: "paragraph",
+                children: [{ type: "text", text: p }],
+              };
+            });
+          })
+        );
+
+        await addNewChapter({ data });
+      }
+    } catch (error) {
+      setError(error?.message);
+      setIsVisible(true);
+      setLoader(false);
     }
   };
 
@@ -76,40 +94,71 @@ const BooksRow = ({ folder, book }) => {
   }, [folder]);
 
   return (
-    <Tr key={folder.path}>
-      <Td>
-        <Typography textColor="neutral800">{folder.id}</Typography>
-      </Td>
-      <Td>
-        <Typography textColor="neutral800">{folder.name}</Typography>
-      </Td>
-      <Td>{publishedChapterOrders?.join(", ") || 0}</Td>
-      <Td>
-        <Typography width="20px" overflow="scroll" textColor="neutral800">
-          {localNames}
-        </Typography>
-      </Td>
-      <Td>
-        <Status
-          variant={isPublished ? "success" : "draft"}
-          size="S"
-          showBullet={false}
-        >
-          <Typography
-            fontWeight="bold"
-            textColor={isPublished ? "success700" : "secondary700"}
-          >
-            {isPublished ? "Published" : "Not Published"}
+    <>
+      <Tr key={folder.path}>
+        <Td>
+          <Typography textColor="neutral800">{folder.id}</Typography>
+        </Td>
+        <Td>
+          <Typography textColor="neutral800">{folder.name}</Typography>
+        </Td>
+        <Td>{publishedChapterOrders?.join(", ") || 0}</Td>
+        <Td>
+          <Typography width="20px" overflow="scroll" textColor="neutral800">
+            {localNames}
           </Typography>
-        </Status>
-      </Td>
-      <Td>
-        {/* <Button onClick={deleteBook} disabled={!isPublished} size="S" width="100px" variant="danger">Delete</Button> */}
-        <Button onClick={buildBook} size="S" width="100px" variant="success">
-          Build
-        </Button>
-      </Td>
-    </Tr>
+        </Td>
+        <Td>
+          <Status
+            variant={isPublished ? "success" : "draft"}
+            size="S"
+            showBullet={false}
+          >
+            <Typography
+              fontWeight="bold"
+              textColor={isPublished ? "success700" : "secondary700"}
+            >
+              {isPublished ? "Published" : "Not Published"}
+            </Typography>
+          </Status>
+        </Td>
+        <Td>
+          {/* <Button onClick={deleteBook} disabled={!isPublished} size="S" width="100px" variant="danger">Delete</Button> */}
+          {loader ? (
+            <Loader small>Loading content...</Loader>
+          ) : (
+            <Button
+              onClick={buildBook}
+              size="S"
+              width="100px"
+              variant="success"
+            >
+              Build
+            </Button>
+          )}
+        </Td>
+      </Tr>
+      <Dialog
+        onClose={() => setIsVisible(false)}
+        title="Confirmation"
+        isOpen={isVisible}
+      >
+        <DialogBody icon={<ExclamationMarkCircle />}>
+          <Flex direction="column" alignItems="center" gap={2}>
+            <Flex justifyContent="center">
+              <Typography id="confirm-description">{error}</Typography>
+            </Flex>
+          </Flex>
+        </DialogBody>
+        <DialogFooter
+          startAction={
+            <Button onClick={() => setIsVisible(false)} variant="tertiary">
+              Cancel
+            </Button>
+          }
+        />
+      </Dialog>
+    </>
   );
 };
 
--- src/plugins/builder/admin/src/utils/getBooks.js
+++ src/plugins/builder/admin/src/utils/getBooks.js
@@ -38,10 +38,11 @@ export const getAllMediaFolders = async () => {
 export const getAllPublishedBooks = async () => {
   try {
     const response = await axios.get(
-      "http://localhost:1337/api/books?populate=*",
+      `${strapi.backendURL}/api/books?populate=*`,
       {
         headers: {
           Authorization:
+            // "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzEyODQwNDE4LCJleHAiOjE3MTU0MzI0MTh9.omh8SJW-9nag4ACex_0dIl3o1TcjmWTPRKVBe2pJ4T8",
             "Bearer 36f2ae66c496112c79967521e8c18a010c6ba9b6983e1e6037ff91c2595738da0706d13340c43f563627fd4d9b719b4d4ad67aaf6893f0732da9d674e846dab076efe3319681c88c8ce5b2f2ec6c1c951da3404701ac725f241f03f367a0d5ecad0185e3b5e7d070dab65d941c948175d0c3a5393254d83555c10916a11062f2",
         },
       }
